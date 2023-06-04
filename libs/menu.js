661b21ca3664a437396b6710002c63c2060f4b48
ifilex <ifilex@gmail.com>
2023-06-04 03:30:29 -0300

    update
    04062023

---
+++ libs/menu.js
@@ -0,0 +1,7 @@
+(function () {
+  $('#menuToggle').on('click', function () {
+    $('#mainmenu').toggleClass('menu-open');
+    //small hack to fix the toggle button position
+    $('#menuToggle').toggleClass('menu-openfix');
+  });
+})(jQuery);
